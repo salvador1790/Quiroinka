f7b16da860f36ea18a511eaa911f5e4758a496b3
Salvador Murillo <chavitation@gmail.com>
2020-07-28 23:07:47 -0700

    first push includes non mobile site and form

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log("it works");
+
+$(document).ready(function(){
+    $(".submit").on("click",function(event){
+
+        let name = $(".name").val();
+        let email = $(".email").val();
+        let message = $("#message").val();
+        let statusMessage = $(".status");
+        statusMessage.empty();
+
+        if(email.length > 5 && email.includes('@') && email.includes('.')){
+            statusMessage.append('<div>Email is Valid</div>');
+        } else{
+            event.preventDefault();
+            statusMessage.append('<div>Email no es Valido</div>');
+        } 
+
+        if(name.length <=0 ){
+            event.preventDefault();
+            statusMessage.append('<div>El Nombre es Requerido</div>');
+        }
+
+        if(message.length <=0){
+            event.preventDefault();
+            statusMessage.append('<div>No hay mensaje Escrito</div>');
+        }
+    })
+})
