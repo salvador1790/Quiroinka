eb1cd47e060002f7534ce3aa2eeb018f7f93a99c
Salvador Murillo <chavitation@gmail.com>
2020-08-02 13:40:53 -0700

    fixed

--- main.js
+++ main.js
@@ -61,7 +61,4 @@ $(document).ready(function(){
     });
   });
 
-  $(window).scroll(function() {
-    var scrolledY = $(window).scrollTop();
-    $('#header').css('background-position', 'left ' + ((scrolledY)) + 'px');
-  });
+ 
